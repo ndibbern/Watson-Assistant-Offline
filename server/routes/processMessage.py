6383fb0e8b9198c1edb20dcb3adf47ea9283a6ba
Michelle Zeitlin <Michelle.Zeitlin@ibm.com>
2018-07-12 15:56:06 -0500

    Added mongoDB routes

--- server/routes/processMessage.py
+++ server/routes/processMessage.py
@@ -1,11 +1,10 @@
-
 import json
 import datetime
 from server import app
 from flask import Flask
 from flask import abort, session, request, redirect
-
 from server.services import *
+from bson.json_util import dumps
 
 initServices(app) # Initialize Services
 
@@ -16,30 +15,45 @@ conversation = service_manager.get('watson-conversation')
 # Find the workspace_id you want to connect this conversation instance with:
 workspace_id = '6ee41931-3476-487d-a9e9-857dbc718cb8'
 
+client = service_manager.get('mongodb')
+db = client.loc_database
+collection = db.loc_collection
+
 # Add new data to the collection
 @app.route('/message')
 def sendMessage():
   msg = request.args.get('msg')
   lat = request.args.get('lat')
-  longi = request.args.get('longi')
+  longi = request.args.get('long')
   uuid = request.args.get('uuid')
   response = conversation.message(workspace_id=workspace_id, input={
 	 'text': msg
   })
   json_str = json.dumps(response)
   resp = json.loads(json_str)
-  response = (resp['output']['text'][0])
+  response = resp['output']['text'][0]
   return response
 
 
 @app.route('/panic', methods=['POST'])
 def panic():
   lat = request.args.get('lat')
-  longi = request.args.get('longi')
+  longi = request.args.get('long')
   uuid = request.args.get('uuid')
-  ts = datetime.datetime.now().timestamp()
+  ts = datetime.datetime.now()
 
-  return uuid
+  data = {
+    "latitude": lat,
+    "longitude": longi,
+    "timestamp": ts,
+    "uuid": uuid
+  }
 
-  
+  collection.insert_one(data)
 
+  return data
+
+
+@app.route('/locations')
+def getLocs():
+  return dumps(collection.find({}, {'_id': 0}))
